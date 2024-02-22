ebd199fc5617be18274a89133ac332ca8f0ffd07
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-02-22 12:40:39 +0100

    Add Join function to strings.hpp

--- src/util/strings.hpp
+++ src/util/strings.hpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <sstream>
+#include <iterator>
 
 namespace Tram
 {
@@ -12,6 +14,24 @@ namespace Tram
         {
             std::transform(str_in.begin(), str_in.end(), str_in.begin(), [](unsigned char c)
                            { return std::tolower(c); });
+        }
+
+        std::string Join(const std::vector<std::string> &vec, const char *delimiter = ", ")
+        {
+            std::ostringstream stream;
+
+            auto begin = vec.begin(), end = vec.end();
+
+            if (begin != end)
+            {
+                std::copy(begin, std::prev(end), std::ostream_iterator<std::string>(stream, delimiter));
+                begin = prev(end);
+            }
+
+            if (begin != end)
+                stream << *begin;
+
+            return stream.str();
         }
     }
 }
