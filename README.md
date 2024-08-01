ca3cbe1b2100805f503ddaae2b28391ec018e699
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-08-01 11:33:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tram - Another simple build configuration tool
---
+++ premake5.lua
@@ -0,0 +1,29 @@
+workspace "Tram"
+    configurations {"Debug", "Release"}
+
+project "Tram"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    architecture "x86_64"
+    targetdir "bin/%{cfg.buildcfg}"
+    location "build/"
+
+    warnings "Extra"
+
+    files {"src/**.hpp", "src/**.cpp"}
+
+    filter "configurations:Debug"
+        filter "system:linux"
+            linkoptions{"-fuse-ld=mold"}
+
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        filter "system:linux"
+            linkoptions{"-fuse-ld=mold"}
+        
+        defines { "NDEBUG" }
+        optimize "On"
+        symbols "Off"
