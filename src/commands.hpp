2bf2d97271bf3f6f649c5a83ac5f304b9508e426
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-08-01 12:27:29 +0200

    Added every subcommand

--- src/commands.hpp
+++ src/commands.hpp
@@ -6,3 +6,18 @@ inline auto HELP_ACTION = [](const psap::ArgParser &parser, [[maybe_unused]] con
 {
     parser(parser[0]);
 };
+
+inline auto NEW_ACTION = []([[maybe_unused]] const auto &_parser, [[maybe_unused]] const auto &_cmd) {
+};
+
+inline auto ADD_ACTION = []([[maybe_unused]] const auto &_parser, [[maybe_unused]] const auto &_cmd) {
+};
+
+inline auto REMOVE_ACTION = []([[maybe_unused]] const auto &_parser, [[maybe_unused]] const auto &_cmd) {
+};
+
+inline auto BUILD_ACTION = []([[maybe_unused]] const auto &_parser, [[maybe_unused]] const auto &_cmd) {
+};
+
+inline auto RUN_ACTION = []([[maybe_unused]] const auto &_parser, [[maybe_unused]] const auto &_cmd) {
+};
--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,31 @@ int main(int argc, char *argv[])
         .help("Displays help for a tram subcommand")
         .action(HELP_ACTION);
 
+    parser.command("new", "n", "create", "c")
+        .help("Creates a new project.")
+        .action(NEW_ACTION);
+
+    parser.command("add", "a")
+        .help("Adds a new dependency.")
+        .option(psap::Option::Value({"--branch", "-b"}, "Select a specific branch."))
+        .option(psap::Option::Flag({"--link", "-l"}, "Links the specified library."))
+        .action(ADD_ACTION);
+
+    parser.command("remove", "rm", "delete", "del")
+        .help("Removes a dependency.")
+        .action(REMOVE_ACTION);
+
+    parser.command("build", "b")
+        .help("Builds the project.")
+        .option(psap::Option::Flag({"--debug", "-d"}, "Runs debug build."))
+        .option(psap::Option::Flag({"--release", "-r"}, "Runs release build."))
+        .action(BUILD_ACTION);
+
+    parser.command("run", "r")
+        .help("Executes the project and perform a build, if needed.")
+        .option(psap::Option::Flag({"--debug", "-d"}, "Runs debug build."))
+        .option(psap::Option::Flag({"--release", "-r"}, "Runs release build."))
+        .action(RUN_ACTION);
+
     parser.parse(argv, argc);
 }
