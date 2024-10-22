707a315413f50cc920ee2aaff2fd5d9d344bc773
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-10-22 21:28:03 +0200

    feat(Commands): Added new command implementation

--- src/commands.hpp
+++ src/commands.hpp
@@ -3,6 +3,11 @@
 #include <arg_parser.hpp>
 #include <toml.hpp>
 
+#include "error/error.hpp"
+#include "utility/utility.hpp"
+
+#include <filesystem>
+#include <format>
 #include <iostream>
 
 namespace tram {
@@ -11,13 +16,29 @@ inline auto HELP_ACTION = [](const psap::ArgParser& parser, [[maybe_unused]] con
     parser(parser[0]);
 };
 
-inline auto NEW_ACTION = [](const psap::ArgParser& parser, [[maybe_unused]] const psap::Command& cmd) noexcept {
-    auto project_name = parser[0];
+inline auto NEW_ACTION = [](const psap::ArgParser& parser, const psap::Command& cmd) noexcept {
+    std::filesystem::path project_name { parser[0] };
 
     if (project_name.empty()) {
         std::cout << "Please specify a name!" << std::endl;
         return;
     }
+
+    if (auto error = fs::create_dir_if_notexists(project_name)) {
+        if (error.is(ErrorCode::DirAlreadyExists))
+            std::cout << psap::color::red << std::format("There is already a directory called '{}'.", project_name.string()) << psap::color::reset << std::endl;
+        else
+            error.report();
+
+        return;
+    }
+
+    fs::create_file(project_name / fs::TRAM_PROJECT_FILE, std::format("name = \"{}\"\n", parser[0]));
+
+    if (cmd["--sample"])
+        fs::create_sample_files(project_name);
+
+    std::cout << "Initialized project in '" << psap::color::light_magenta << std::format("./{}", project_name.string()) << psap::color::reset << "'" << std::endl;
 };
 
 inline auto ADD_ACTION = []([[maybe_unused]] const auto& _parser, [[maybe_unused]] const auto& _cmd) {
