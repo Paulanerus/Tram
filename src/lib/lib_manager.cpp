b4e8966686cb6a32a440b38b85bc33ecd59517ad
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-11-17 17:48:48 +0100

    feat: Implemented 'validate_install'

--- src/lib/lib_manager.cpp
+++ src/lib/lib_manager.cpp
@@ -1,20 +1,37 @@
 #include "lib_manager.hpp"
 
-#include <iostream>
+#include "../error/error.hpp"
+#include "../utility/utility.hpp"
+
+#include <filesystem>
+#include <system_error>
 
 namespace tram {
 namespace lib {
 
     bool validate_install(const tram::internal::library& lib) noexcept
     {
-        return true;
+        const std::filesystem::path lib_path { fs::TRAM_LIBS };
+
+        std::error_code ec;
+
+        auto exists = std::filesystem::exists(lib_path / lib.name, ec);
+
+        if (ec) {
+            auto err = make_systemerror(ec);
+            err.report();
+
+            return false;
+        }
+
+        return exists;
     }
 
-    void install_lib(const tram::internal::library& lib) noexcept
+    void install_lib([[maybe_unused]] const tram::internal::library& lib) noexcept
     {
     }
 
-    void remove_lib(const tram::internal::library& lib) noexcept
+    void remove_lib([[maybe_unused]] const tram::internal::library& lib) noexcept
     {
     }
 }
